14e433a1355cecec1c8d1214052127583b036f39
주희 <84782877+jhuee@users.noreply.github.com>
2023-03-26 23:51:41 +0900

    Document 엔티티 설계 maven 설정~

--- src/main/java/com/project/studyseller/entity/Document.java
+++ src/main/java/com/project/studyseller/entity/Document.java
@@ -1,30 +1,56 @@
 package com.project.studyseller.entity;
 
-import com.project.studyseller.constant.ItemSellPrice;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import javax.persistence.*;
+import javax.persistence.Entity;
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "Document")
 @Getter
 @Setter
 @ToString
 
 public class Document {
+    @Id
+    @Column(name = "Doc_Id")
+    @GeneratedValue(strategy = GenerationType.AUTO)     //JPA 구현체가 자동으로 생성 전략
+    private Long DocSN;    //문서 코드
 
-    private Long id;    //문서 코드
 
-    private String DocNm;   //문서명
+    @Column(nullable = false, length = 50)
+    private String DocTitle;   //문서명
 
+    @Column(nullable = false)
+    private Integer category;   //문서 카테고리
+
+    @CreatedDate        //Insert 시 시간 자동 저장
+    @UpdateTimestamp        //Update 시 시간 자동 저장
+    @Column(nullable = false)
+    private String DocContent;  //문서 본문
+
+    @Column(name = "price", nullable = false)
     private int price;  //가격
 
+    @Lob
+    @Column(nullable = true)
     private String DocDetail;  //문서 부가 설명
 
-    private ItemSellPrice itemSellPrice;    //문서 가격 상태
+    @Column(nullable = false)
+    private Boolean itemSellPrice;    //유무료 결정
 
+
+    @Column(nullable = false)
     private Boolean DocPrivate; //문서 공개 여부
-
+    @Column(nullable = false)
     private LocalDateTime regTime;  //등록 시간
-
+    @Column()
     private LocalDateTime updateTime;   //수정시간
+
+
 }
